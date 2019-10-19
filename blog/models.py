e18b8bb6b24c69e61043df1f2d6b101ee4577952
John Bert <jbertlik@gmail.com>
2019-10-19 13:19:23 -0400

    migrated Post class to the database, and displayed that information to the frontend.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    #auto_now_add=True sets the time to when this post was created and cannot be changed.
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    # User model class is the foreign key for Post model class.
+
+    # on_delete=models.CASCADE tells Django that when an author is deleted, their posts are deleted as well.
+
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
-posts = [
-    {
-        'author': 'Jan Bertlik',
-        'title': 'Blog post 1',
-        'content': 'This is the first post',
-        'date_posted': 'October 17th 2019'
-    },
-    {
-        'author': 'Nikola Telsa',
-        'title': 'Blog post 2',
-        'content': 'This is the second post',
-        'date_posted': 'October 18th 2019'
-    },
-]
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 

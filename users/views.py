d443d137033fbb648e52dfa1f83428a088c09d42
John Bert <jbertlik@gmail.com>
2019-10-23 13:50:28 -0400

    added the ability for users to log in and out.

--- users/views.py
+++ users/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from .forms import UserRegisterForm
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 def register(request):
     if request.method == 'POST':
@@ -8,8 +10,29 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Welcome {username}, you are now registered!')
-            return redirect('blog-home')
+            messages.success(request, f'Welcome {username}, you are now registered! Please log in.')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
+
+def login(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        messages.success(request, f'Welcome back {username}.')
+        return redirect('blog-home')
+    else:
+        messages.error(request, f'Sorry, login failed. Please try again.')
+    return render(request, 'users/login.html')
+
+def logout(request):
+    logout(request)
+    messages.success(request, f'You are now logged out.')
+    return redirect('blog-home')
